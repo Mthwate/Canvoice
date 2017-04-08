d3d30b033accf0a5bdeb4ed2d7aaee5ad7da57e1
Matthew Benkart <mthwate@mthwate.com>
2017-04-08 19:31:07 -0400

    Added test responses

--- src/main/java/com/canvoice/CanvoiceSpeechlet.java
+++ src/main/java/com/canvoice/CanvoiceSpeechlet.java
@@ -8,6 +8,7 @@ import com.amazon.speech.speechlet.SessionStartedRequest;
 import com.amazon.speech.speechlet.Speechlet;
 import com.amazon.speech.speechlet.SpeechletException;
 import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
 
 /**
  * @author mthwate
@@ -21,12 +22,16 @@ public class CanvoiceSpeechlet implements Speechlet {
 
 	@Override
 	public SpeechletResponse onLaunch(LaunchRequest request, Session session) throws SpeechletException {
-		return null;
+		PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+		outputSpeech.setText("Yo, what is up bro?");
+		return SpeechletResponse.newTellResponse(outputSpeech);
 	}
 
 	@Override
 	public SpeechletResponse onIntent(IntentRequest request, Session session) throws SpeechletException {
-		return null;
+		PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+		outputSpeech.setText("I have no clue man.");
+		return SpeechletResponse.newTellResponse(outputSpeech);
 	}
 
 	@Override
