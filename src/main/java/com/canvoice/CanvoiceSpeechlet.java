892114e8f809382f88094e0d4ef7d9302db64f3c
Matthew Benkart <mthwate@mthwate.com>
2017-04-08 19:41:28 -0400

    Upgraded the speechlet to v2.

--- src/main/java/com/canvoice/CanvoiceSpeechlet.java
+++ src/main/java/com/canvoice/CanvoiceSpeechlet.java
@@ -1,41 +1,40 @@
 package com.canvoice;
 
+import com.amazon.speech.json.SpeechletRequestEnvelope;
 import com.amazon.speech.speechlet.IntentRequest;
 import com.amazon.speech.speechlet.LaunchRequest;
-import com.amazon.speech.speechlet.Session;
 import com.amazon.speech.speechlet.SessionEndedRequest;
 import com.amazon.speech.speechlet.SessionStartedRequest;
-import com.amazon.speech.speechlet.Speechlet;
-import com.amazon.speech.speechlet.SpeechletException;
 import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.speechlet.SpeechletV2;
 import com.amazon.speech.ui.PlainTextOutputSpeech;
 
 /**
  * @author mthwate
  */
-public class CanvoiceSpeechlet implements Speechlet {
+public class CanvoiceSpeechlet implements SpeechletV2 {
 
 	@Override
-	public void onSessionStarted(SessionStartedRequest request, Session session) throws SpeechletException {
+	public void onSessionStarted(SpeechletRequestEnvelope<SessionStartedRequest> request) {
 
 	}
 
 	@Override
-	public SpeechletResponse onLaunch(LaunchRequest request, Session session) throws SpeechletException {
+	public SpeechletResponse onLaunch(SpeechletRequestEnvelope<LaunchRequest> request) {
 		PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
 		outputSpeech.setText("Yo, what is up bro?");
 		return SpeechletResponse.newTellResponse(outputSpeech);
 	}
 
 	@Override
-	public SpeechletResponse onIntent(IntentRequest request, Session session) throws SpeechletException {
+	public SpeechletResponse onIntent(SpeechletRequestEnvelope<IntentRequest> request) {
 		PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
 		outputSpeech.setText("I have no clue man.");
 		return SpeechletResponse.newTellResponse(outputSpeech);
 	}
 
 	@Override
-	public void onSessionEnded(SessionEndedRequest request, Session session) throws SpeechletException {
+	public void onSessionEnded(SpeechletRequestEnvelope<SessionEndedRequest> request) {
 
 	}
 
