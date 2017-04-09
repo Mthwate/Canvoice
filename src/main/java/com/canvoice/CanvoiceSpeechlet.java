5850c0c508d0be181d7f73d8c3a45090f719a3ce
jahardner <johannaahardner@gmail.com>
2017-04-08 22:17:33 -0400

    output functions

--- src/main/java/com/canvoice/CanvoiceSpeechlet.java
+++ src/main/java/com/canvoice/CanvoiceSpeechlet.java
@@ -55,5 +55,40 @@ public class CanvoiceSpeechlet implements SpeechletV2 {
 	public void onSessionEnded(SpeechletRequestEnvelope<SessionEndedRequest> request) {
 
 	}
+	
+	private SpeechletResponse toAlexa(string output) {
+        // Create the Simple card content.
+        SimpleCard card = new SimpleCard();
+        card.setTitle("CanVoice");
+        card.setContent(output);
+
+        // Create the plain text output.
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(output);
+
+        return SpeechletResponse.newTellResponse(speech, card);
+    }
+	
+	private void output(Course c, Assignment[] ass) {
+		string out = "In " + c.ToString() + " you have ";
+		for (int i = 0; i < ass.length; i++) {
+			out += ass[i].toString() + " coming up on " + ass[i].due_at;
+		}
+		toAlexa(out);
+	}
+		
+	private void output(Course c, Assignment ass) {
+		toAlexa("Your grade on " + ass.ToString() + " in " + c.ToString() + " is " + ass.submission.grade);
+	}
+
+	private void output(Course c) {
+		toAlexa("Your grade in " + c.ToString() + " is " + c.enrollments[0].computed_current_grade());
+	}
+
+	private void output(Course[] c) {
+		for (int i = 0; i < c.length; i++) {
+			output(c[i]);
+		}
+	}
 
 }
